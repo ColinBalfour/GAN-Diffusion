751e45c7c0fd6800f9257270f2a6578e64588456
felixng <ngthinh302@gmail.com>
2024-10-10 23:43:19 -0400

    correct augmented mlp training

--- src/ImageClassification/utils.py
+++ src/ImageClassification/utils.py
@@ -9,7 +9,7 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 
 class Pipeline:
-    def __init__(self, synthetic_data_loader=None):
+    def __init__(self, synthetic_dataset=None):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
         transform = transforms.Compose(
@@ -25,27 +25,40 @@ class Pipeline:
         trainset = torchvision.datasets.CIFAR10(
             root="./data", train=True, download=True, transform=transform
         )
-        original_trainloader = torch.utils.data.DataLoader(
-            trainset, batch_size=32, shuffle=True, num_workers=2
-        )
 
-        # if synthetic_data_loader is not None:
-        #     # Combine original CIFAR-10 data with synthetic data
-        #     self.trainloader = torch.utils.data.ConcatDataset(
-        #         [original_trainloader.dataset, synthetic_data_loader.dataset]
-        #     )
-        # else:
-        # Only use original data if no synthetic data is provided
-        self.trainloader = original_trainloader.dataset
+        if synthetic_dataset is not None:
+            # Combine original CIFAR-10 data with synthetic data
+            concat_dataset = torch.utils.data.ConcatDataset(
+                [trainset, synthetic_dataset]
+            )
+            self.trainloader = torch.utils.data.DataLoader(
+                concat_dataset, batch_size=32, shuffle=True, num_workers=4
+            )
+        else:
+            # Only use original data if no synthetic data is provided
+            self.trainloader = torch.utils.data.DataLoader(
+                trainset, batch_size=32, shuffle=True, num_workers=4
+            )
 
         testset = torchvision.datasets.CIFAR10(
             root="./data", train=False, download=True, transform=transform
         )
         self.testloader = torch.utils.data.DataLoader(
-            testset, batch_size=32, shuffle=False, num_workers=2
+            testset, batch_size=32, shuffle=False, num_workers=4
         )
 
-        self.classes = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        self.classes = (
+            "plane",
+            "car",
+            "bird",
+            "cat",
+            "deer",
+            "dog",
+            "frog",
+            "horse",
+            "ship",
+            "truck",
+        )
 
         self.lossFunc = nn.CrossEntropyLoss()
 
@@ -55,7 +68,7 @@ class Pipeline:
         epochloss = 0
         for batchcount, (images, labels) in enumerate(self.trainloader):
             images = images.to(self.device)
-            labels = torch.tensor([labels]).to(self.device)
+            labels = labels.to(self.device)
 
             # labels = torch.eye(len(classes))[labels].to(device)
             # pdb.set_trace()
